d70c0e883ccc551821aaa97516838a2516a6adf6
serialt <t@imau.cc>
2022-11-21 18:26:44 +0800

    fix: 去掉无用插件

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 # Email         : serialt@qq.com
 # Github        : https://github.com/serialt
 # Created Time  : 2022-02-17 06:55:27
-# Last modified : 2022-11-21 17:32:38
+# Last modified : 2022-11-21 18:25:56
 # FilePath      : /vscode/build.sh
 # Other         :
 #               :
@@ -96,8 +96,10 @@ InstallDEV_ENV() {
         cd /tmp/
         wget https://go.dev/dl/go${IMAU_GO_VERSION}.linux-amd64.tar.gz
         tar -xf go*.tar.gz -C /usr/local
+        rm -rf  go*.tar.gz
         wget https://github.com/coder/code-server/releases/download/v${IMAU_VSCODE_VERSION}/code-server-${IMAU_VSCODE_VERSION}-linux-amd64.tar.gz
         tar -xf code-server*.tar.gz
+        rm -rf code-server*.tar.gz
         mv /tmp/code-server-${IMAU_VSCODE_VERSION}-linux-amd64 /opt/code-server
         wget -O /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v${IMAU_DUMP_INIT}/dumb-init_${IMAU_DUMP_INIT}_x86_64
         chmod +x /usr/local/bin/dumb-init
@@ -107,7 +109,6 @@ InstallDEV_ENV() {
 
 Install_extension() {
     extension_list=(
-        766b.go-outliner
         alefragnani.Bookmarks
         alefragnani.project-manager
         alexcvzz.vscode-sqlite
@@ -122,20 +123,15 @@ Install_extension() {
         eamodio.gitlens
         felipecaputo.git-project-manager
         formulahendry.code-runner
-        formulahendry.terminal
+
         foxundermoon.shell-format
         golang.go
         Gruntfuggly.todo-tree
-        howardzuo.vscode-git-tags
-        huizhou.githd
         humao.rest-client
-        IJustDev.gitea-vscode
         ionutvmi.path-autocomplete
         ipedrazas.kubernetes-snippets
-        lunuan.kubernetes-templates
         matthewpi.caddyfile-support
         mhutchie.git-graph
-        mishkinf.goto-next-previous-member
         mrmlnc.vscode-apache
         ms-azuretools.vscode-docker
         MS-CEINTL.vscode-language-pack-zh-hans
@@ -143,37 +139,20 @@ Install_extension() {
         ms-toolsai.jupyter
         ms-toolsai.jupyter-keymap
         ms-toolsai.jupyter-renderers
-        ms-vscode-remote.remote-ssh
-        ms-vscode-remote.remote-ssh-edit
-        ms-vscode-remote.vscode-remote-extensionpack
         ms-vscode.cmake-tools
         mtxr.sqltools
         njpwerner.autodocstring
-        nkjoep.mac-classic-theme
         OBKoro1.korofileheader
-        Okteto.remote-kubernetes
-        r3inbowari.gomodexplorer
         redhat.vscode-yaml
         Remisa.shellman
-        sandipchitale.vscode-kubernetes-helm-extras
-        shaharkazaz.git-merger
-        shanoor.vscode-nginx
-        technosophos.vscode-helm
-        Tim-Koehler.helm-intellisense
-        tomaciazek.ansible
-        truman.autocomplate-shell
         twxs.cmake
-        Tyriar.terminal-tabs
-        VisualStudioExptTeam.vscodeintellicode
         vscode-icons-team.vscode-icons
         waderyan.gitblame
         wholroyd.jinja
-        william-voyek.vscode-nginx
-        xmtt.go-mod-grapher
+
         yzhang.markdown-all-in-one
-        ZainChen.json
-        zamerick.vscode-caddyfile-syntax
-        zbr.vscode-ansible
+
+
     )
 
     for aobj in ${extension_list[@]}; do
