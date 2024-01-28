81e65dc2e6dc1242a7860c00def9128d9d50e166
serialt <tserialt@gmail.com>
2024-01-28 10:08:08 +0000

    vscode v4.20.1 (Github Actions Automatically Built in 2024-01-28 10:08)

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@
 # ***********************************************************************
 
 
-export IMAU_VSCODE_VERSION=4.20.0
+export IMAU_VSCODE_VERSION=4.20.1
 
 export GO_SDK_DIR=/root/sdk
 export CODE_ARCH=amd64
